d9bec682bee2a3ca027d96090a41a14d20d5c967
SharonMai <54488339+MaiSharon@users.noreply.github.com>
2024-12-16 04:52:54 +0800

    fix: 修正返回類型
    - 刪除包裝型別在其他分層處理錯處機制
    - 修正響應的數據類名稱  NoteResponse -> NoteDto

--- app/src/main/java/com/tibame/foodhunter/sharon/data/datasource/remote/api/ApiService.kt
+++ app/src/main/java/com/tibame/foodhunter/sharon/data/datasource/remote/api/ApiService.kt
@@ -1,14 +1,12 @@
 package com.tibame.foodhunter.sharon.data.datasource.remote.api
 
-import com.tibame.foodhunter.sharon.data.datasource.remote.model.NoteResponse
-import retrofit2.Response
+import com.tibame.foodhunter.sharon.data.datasource.remote.model.NoteDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface NoteApiService {
-    @GET("api/members/{memberId}/notes")
-    // Response是http狀態響應
+    @GET("members/{memberId}/notes")
     suspend fun getNotes(
         @Path("memberId") memberId: String
-    ) : Response<List<NoteResponse>>  // Response的庫要使用retrofit2
+    ): List<NoteDto>
 }
