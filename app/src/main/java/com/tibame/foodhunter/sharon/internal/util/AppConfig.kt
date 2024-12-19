f49261b03f91b3bcc9ef875b3752680948682eda
SharonMai <54488339+MaiSharon@users.noreply.github.com>
2024-12-20 03:52:25 +0800

    refractor: delete not use Result and app config

--- app/src/main/java/com/tibame/foodhunter/sharon/internal/util/AppConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.tibame.foodhunter.sharon.internal.util
-
-import com.tibame.foodhunter.BuildConfig
-
-object AppConfig{
-    val isTestMode = BuildConfig.DEBUG
-
-    val apiUrl = BuildConfig.BASE_URL
-}
