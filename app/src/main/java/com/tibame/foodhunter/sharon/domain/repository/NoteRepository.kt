d763bcec15bae3e1d0a96b10664b21677e76f2bd
SharonMai <54488339+MaiSharon@users.noreply.github.com>
2024-12-20 03:52:50 +0800

    fix: not use flow

--- app/src/main/java/com/tibame/foodhunter/sharon/domain/repository/NoteRepository.kt
+++ app/src/main/java/com/tibame/foodhunter/sharon/domain/repository/NoteRepository.kt
@@ -1,12 +1,13 @@
 package com.tibame.foodhunter.sharon.domain.repository
 
 import com.tibame.foodhunter.sharon.domain.entity.Note
-import com.tibame.foodhunter.sharon.data.datasource.remote.model.Result
+import com.tibame.foodhunter.sharon.domain.error.DataError
+import com.tibame.foodhunter.sharon.domain.error.Result
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 interface NoteRepository {
-    val notes: StateFlow<List<Note>>
+//    val notes: StateFlow<List<Note>>
 
-    suspend fun getNotes(memberId: String): Flow<Result<List<Note>>> // Response的庫要使用retrofit2
+    suspend fun getNotes(memberId: String): Result<List<Note>, DataError.Network>
 }
